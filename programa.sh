29577ebc468f66ea566d94a6845ac8eef0a9e729
Kotryna Šlepetytė <49883485+sniegapele@users.noreply.github.com>
2021-05-01 21:49:35 +0300

    Update programa.sh

--- programa.sh
+++ programa.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-#SBATCH -p short # eilė
-#SBATCH -N1 # keliuose kompiuteriuose (trečioje programoje nenaudojame MPI, todėl tik 1)
-#SBATCH -c12 # kiek procesorių viename kompiuteryje
-#SBATCH -C beta # telkinys (jei alpha neveikia, bandykite beta)
-java Main # jūsų programos įjungimas
+#SBATCH -p short 
+#SBATCH -N1 
+#SBATCH -c12 
+#SBATCH -C beta 
+java Main 
