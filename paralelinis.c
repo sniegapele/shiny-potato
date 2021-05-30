454f1706f2b2115e19e91c1a0c7a94b3dd049db5
Kotryna Šlepetytė <49883485+sniegapele@users.noreply.github.com>
2021-05-30 14:06:17 +0300

    Add files via upload

--- paralelinis.c
+++ paralelinis.c
@@ -12,27 +12,27 @@
 #define WORKTAG    1
 #define DIETAG     2
 
-void master(long upperBound);
+void master(int upperBound);
 
 void slave();
 
-void generatePrimeNumbers(long *numberOfNumbers, long upperBound, long *primes);
+void generatePrimeNumbers(int *numberOfNumbers, int upperBound, int *primes);
 
 int main(int argc, char *argv[]) {
-    long upperBound = atol(argv[1]);
+    int upperBound = atoi(argv[1]);
 
     if (upperBound <= 2) {
         printf("Invalid arguments!");
         return 0;
     }
 
-    int myrank;
+    int myRank;
 
     MPI_Init(&argc, &argv);         /* initialize MPI */
     MPI_Comm_rank(MPI_COMM_WORLD,   /* always use this */
-                  &myrank);         /* process rank, 0 thru N-1 */
+                  &myRank);         /* process rank, 0 thru N-1 */
 
-    if (myrank == 0) {
+    if (myRank == 0) {
         master(upperBound);
     } else {
         slave();
@@ -40,15 +40,15 @@ int main(int argc, char *argv[]) {
     MPI_Finalize();                 /* cleanup MPI */
 }
 
-void master(long upperBound) {
+void master(int upperBound) {
     double startTime = (double) clock();
 
-    long numberOfPrimes;
-    long primesToSqrt[(long) sqrt(upperBound)];
+    int numberOfPrimes;
+    int primesToSqrt[(int) sqrt(upperBound)];
     generatePrimeNumbers(&numberOfPrimes, upperBound, primesToSqrt);
 
     int ntasks, rank;
-    long result = numberOfPrimes;
+    int result = numberOfPrimes;
     MPI_Status status;
 
     MPI_Comm_size(
@@ -57,18 +57,18 @@ void master(long upperBound) {
 
     // send info about already calculated primes
     for (rank = 1; rank < ntasks; ++rank) {
-        MPI_Send(&numberOfPrimes, 1, MPI_LONG, rank, WORKTAG, MPI_COMM_WORLD);
-        MPI_Send(primesToSqrt, numberOfPrimes, MPI_LONG, rank, WORKTAG, MPI_COMM_WORLD);
+        MPI_Send(&numberOfPrimes, 1, MPI_INT, rank, WORKTAG, MPI_COMM_WORLD);
+        MPI_Send(primesToSqrt, numberOfPrimes, MPI_INT, rank, WORKTAG, MPI_COMM_WORLD);
     }
 
-    long numberOfTasks = ntasks - 1;
+    int numberOfTasks = ntasks - 1;
 
-    long numberOfNumbers = (upperBound - (long) sqrt(upperBound)) / numberOfTasks - 1;
-    long leftovers = (upperBound - (long) sqrt(upperBound)) % numberOfTasks;
+    int numberOfNumbers = (upperBound - (int) sqrt(upperBound)) / numberOfTasks - 1;
+    int leftovers = (upperBound - (int) sqrt(upperBound)) % numberOfTasks;
 
-    long startNumber = (long) sqrt(upperBound) + 1;
+    int startNumber = (int) sqrt(upperBound) + 1;
 
-    long message[2];
+    int message[2];
 
     for (rank = 1; rank <= numberOfTasks; rank++) {
         // bounds calculation
@@ -88,13 +88,13 @@ void master(long upperBound) {
         }
 
         // send bounds
-        MPI_Send(message, 2, MPI_LONG, rank, WORKTAG, MPI_COMM_WORLD);
+        MPI_Send(message, 2, MPI_INT, rank, WORKTAG, MPI_COMM_WORLD);
     }
 
     // receive results
-    for (long i = 0; i < numberOfTasks; i++) {
-        long receivedResult;
-        MPI_Recv(&receivedResult, 1, MPI_LONG, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+    for (int i = 0; i < numberOfTasks; i++) {
+        int receivedResult;
+        MPI_Recv(&receivedResult, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
         result += receivedResult;
     }
 
@@ -102,7 +102,7 @@ void master(long upperBound) {
         MPI_Send(0, 0, MPI_INT, rank, DIETAG, MPI_COMM_WORLD);
     }
 
-    printf("Found %ld primes in range [2, %ld]\n", result, upperBound);
+    printf("Found %d primes in range [2, %d]\n", result, upperBound);
     double endTime = (double) clock();
     printf("Time: %.5f\r\n", (endTime - startTime) / CLOCKS_PER_SEC);
 }
@@ -111,26 +111,26 @@ void slave() {
     MPI_Status status;
 
     // receive minimum primes
-    long numberOfItems;
-    MPI_Recv(&numberOfItems, 1, MPI_LONG, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-    long primes[numberOfItems];
-    MPI_Recv(&primes, numberOfItems, MPI_LONG, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+    int numberOfItems;
+    MPI_Recv(&numberOfItems, 1, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+    int primes[numberOfItems];
+    MPI_Recv(&primes, numberOfItems, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
 
-    long bounds[2];
-    MPI_Recv(&bounds, 2, MPI_LONG, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+    int bounds[2];
+    MPI_Recv(&bounds, 2, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
 
-    long numberOfNumberInInterval = bounds[1] - bounds[0] + 1;
+    int numberOfNumberInInterval = bounds[1] - bounds[0] + 1;
 
     // init current interval
-    long myInterval[numberOfNumberInInterval];
-    for (long i = 0; i < numberOfNumberInInterval; i++) {
+    int myInterval[numberOfNumberInInterval];
+    for (int i = 0; i < numberOfNumberInInterval; i++) {
         myInterval[i] = true;
     }
 
     // calculate primes in interval
-    for (long i = 0; i < numberOfItems; i++) {
-        long start;
-        long coef = (bounds[0] - primes[i] * primes[i]) / primes[i];
+    for (int i = 0; i < numberOfItems; i++) {
+        int start;
+        int coef = (bounds[0] - primes[i] * primes[i]) / primes[i];
 
         if (coef < 0) {
             start = primes[i] * primes[i];
@@ -140,48 +140,48 @@ void slave() {
             start = primes[i] * primes[i] + coef * primes[i];
         }
 
-        for (long j = start; j <= bounds[1]; j += primes[i]) {
+        for (int j = start; j <= bounds[1]; j += primes[i]) {
             myInterval[j - bounds[0]] = false;
         }
     }
 
     // count calculated prime numbers in given interval
-    long count = 0;
-    for (long i = 0; i < numberOfNumberInInterval; i++) {
+    int count = 0;
+    for (int i = 0; i < numberOfNumberInInterval; i++) {
         if (myInterval[i]) {
             count++;
         }
     }
 
-    MPI_Send(&count, 1, MPI_LONG, 0, WORKTAG, MPI_COMM_WORLD);
+    MPI_Send(&count, 1, MPI_INT, 0, WORKTAG, MPI_COMM_WORLD);
 
     // receive end tag
     int randomPlace;
     MPI_Recv(&randomPlace, 1, MPI_INT, 0, DIETAG, MPI_COMM_WORLD, &status);
 }
 
-void generatePrimeNumbers(long *numberOfNumbers, long upperBound, long *primes) {
-    long sqrtFromUpperBound = sqrt(upperBound);
+void generatePrimeNumbers(int *numberOfNumbers, int upperBound, int *primes) {
+    int sqrtFromUpperBound = (int) sqrt(upperBound);
 
     // init array
     bool interval[sqrtFromUpperBound - 1];
-    for (long i = 0; i < sqrtFromUpperBound - 1; i++) {
+    for (int i = 0; i < sqrtFromUpperBound - 1; i++) {
         interval[i] = true;
     }
 
     // calculate primes until sqrt(upperBound) reached
-    long sqrtOfSqrt = sqrt(sqrtFromUpperBound);
-    for (long i = 2; i <= sqrtOfSqrt; i++) {
+    int sqrtOfSqrt = (int) sqrt(sqrtFromUpperBound);
+    for (int i = 2; i <= sqrtOfSqrt; i++) {
         if (interval[i - 2]) {
-            for (long j = i * i; j <= sqrtFromUpperBound; j += i) {
+            for (int j = i * i; j <= sqrtFromUpperBound; j += i) {
                 interval[j - 2] = false;
             }
         }
     }
 
     // count prime numbers
-    long number = 0;
-    for (long i = 0; i < sqrtFromUpperBound - 1; i++) {
+    int number = 0;
+    for (int i = 0; i < sqrtFromUpperBound - 1; i++) {
         if (interval[i]) {
             number++;
         }
@@ -190,7 +190,7 @@ void generatePrimeNumbers(long *numberOfNumbers, long upperBound, long *primes) 
     // pass number of prime numbers in that interval for master
     *numberOfNumbers = number;
 
-    long index = 0;
+    int index = 0;
     for (int i = 0; i < sqrtFromUpperBound - 1; i++) {
         if (interval[i]) {
             primes[index] = i + 2;
